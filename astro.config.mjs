be2da757ef2f7c49cbc0faeadfd281640dcec171
Jacobo Vega-Albela <jvegaalbela@gmail.com>
2026-05-01 09:53:21 -0400

    set up github pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,6 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-     integrations: [],
-   });
+ site: 'https://jvegaalbela-png.github.io',
+ integrations: [],
+});
