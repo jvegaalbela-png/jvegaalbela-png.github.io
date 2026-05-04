0b8e45c962220b7a581ce93b40e334db64f194ae
Jacobo Vega-Albela <jvegaalbela@gmail.com>
2026-05-04 11:34:18 -0400

    add sitemap and robots.txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,10 @@ import { defineConfig } from 'astro/config';
 
 import mdx from '@astrojs/mdx';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
- site: 'https://jvegaalbela-png.github.io',
- integrations: [mdx()],
+ site: 'https://jva-music.com',
+ integrations: [mdx(), sitemap()],
 });
